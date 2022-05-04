b5701e5303cd3c104c5cb2d00c9f7e5d9c1ea168
Deceit-87 <manuelfreund318@gmail.com>
2022-05-04 20:24:10 +0200

    annidamento route admin

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+ ister web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
@@ -19,4 +19,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+->name('admin.')
+->prefix('admin')
+->namespace('Admin')
+->group(function(){
+    Route::get('/home', 'HomeController@index')->name('home');
+    // Route::get('/contact','HomeController@index');
+    Route::resource('posts','PostController');
+
+
+});
