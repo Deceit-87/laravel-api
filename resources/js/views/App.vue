76a84e93a18debb74bd94cc88b6827b0d16f597d
Deceit-87 <manuelfreund318@gmail.com>
2022-05-11 17:10:29 +0200

    component post

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,12 +1,26 @@
 <template>
 <div>
-    App.vue
+   <header>
+       <ul>
+           <li>Home</li>
+           <li>Posts</li>
+           <li>Categories</li>
+       </ul>
+   </header>
+   <PostsGrid/>
+   <footer> Boolpress 2022
+       &copy;
+   </footer>
 </div>
   
 </template>
 
 <script>
+import PostGrid from'./Posts.index.vue'
 export default {
+    components:{
+        PostGrid,
+    }
 
 }
 </script>
---
+++ resources/js/views/Posts.index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <div class="container">
+          <div v-for="post in Posts" :key="post.id">
+
+              {{ post.title }}
+
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            posts:[]
+        }
+    },
+    methods:{
+        fetchPosts({
+          
+
+        })
+    },
+    mounted(){
+        this.fetchPosts();
+    }
+
+}
+</script>
+
+<style>
+
+</style>
